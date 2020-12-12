943aa66d8bdd9e7546ff4a9884e3ff67da02fdbf
Fergus Molloy <fdm504@york.ac.uk>
2020-12-12 00:04:17 +0000

    started on analying program

---
+++ src/main.rs
@@ -0,0 +1,96 @@
+use colored::Colorize;
+use std::io::{self, Read};
+mod task;
+
+fn main() {
+    // leaving main clear for arg parsing
+    let mut tasks: Vec<task::Task> = Vec::new();
+    get_inp(&mut tasks);
+    // print all tasks so user can check details
+    for i in tasks.iter() {
+        println!("{}\n", i);
+    }
+    // print if schedulable
+    let (util, max_util) = util(&tasks);
+    if util <= max_util {
+        println!(
+            "max util: {:.4}\ntotal util: {:.4}\n{}",
+            max_util,
+            util,
+            "schedulable? true\n".green().bold()
+        );
+    } else {
+        println!(
+            "max util: {:.4}\ntotal util: {:.4}\n{}",
+            max_util,
+            util,
+            "schedulable? false\n".red().bold()
+        );
+    }
+
+    for x in tasks.iter() {
+        println!("Task {} has response time: {}", x.name, x.r);
+    }
+}
+
+fn get_inp(tasks: &mut Vec<task::Task>) {
+    // read in tasks
+    let mut inp = String::new();
+    io::stdin()
+        .read_to_string(&mut inp)
+        .expect("unable to read line");
+
+    // for each task
+    for x in inp.lines().skip(1) {
+        // split by , since csv
+        let mut iter = x.split(',');
+        // items should be in this order since using template
+        // need to do this outside of assignment so i can calculate u
+        let name = iter.next().unwrap().trim().to_string();
+        let t = iter.next().unwrap().parse().unwrap();
+        let c = iter.next().unwrap().parse().unwrap();
+        let p = iter.next().unwrap().parse().unwrap();
+        let u = c as f64 / t as f64;
+        let r = 0;
+        // create task and push to vec
+        tasks.push(task::Task {
+            name,
+            t,
+            c,
+            p,
+            u,
+            r,
+        });
+    }
+    let cloned = tasks.clone();
+    for x in tasks.into_iter() {
+        x.r = response(x, &cloned) as u32;
+    }
+}
+
+fn util(tasks: &Vec<task::Task>) -> (f64, f64) {
+    let n = tasks.len() as f64;
+    let max_util = n * ((2 as f64).powf(1 as f64 / n) - 1 as f64);
+    (tasks.iter().map(|x| x.u).sum(), max_util)
+}
+
+fn response(task: &task::Task, tasks: &Vec<task::Task>) -> f64 {
+    //get higher priority tasks
+    let higher_p: Vec<task::Task> = tasks
+        .iter()
+        .filter(|x| x.p > task.p)
+        .map(|x| x.clone())
+        .collect();
+
+    let mut r = 0.0;
+    let mut last_r = -1.0;
+    while r != last_r {
+        last_r = r;
+        r = task.c as f64
+            + higher_p
+                .iter()
+                .map(|j| (r as f64 / j.t as f64).ceil() * j.c as f64)
+                .sum::<f64>();
+    }
+    r
+}
---
+++ src/task.rs
@@ -0,0 +1,45 @@
+use std::cmp::Ordering;
+use std::fmt;
+
+#[derive(Clone)]
+pub struct Task {
+    pub name: String,
+    pub t: u32,
+    pub c: u32,
+    pub p: u32,
+    pub u: f64,
+    pub r: u32,
+}
+
+impl std::fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Task {}\nt: {t}\nc: {c}\np: {p}\nu: {u}",
+            self.name,
+            t = self.t,
+            c = self.c,
+            p = self.p,
+            u = self.u
+        )
+    }
+}
+
+impl PartialEq for Task {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name
+    }
+}
+impl Eq for Task {}
+
+impl PartialOrd for Task {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Task {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.p.cmp(&other.p)
+    }
+}
