cdfdb04b703014cccc56cfc167a3bf3cbcdd49d7
Fergus Molloy <fergus@molloy.xyz>
2021-01-23 11:51:24 +0000

    Added automatic priority ordering closes #2

--- src/main.rs
+++ src/main.rs
@@ -5,48 +5,52 @@ mod task;
 
 fn main() {
     let tasks = get_inp("tasks", None);
-    for x in tasks {
-        print!("{}", x);
+    for x in tasks.iter() {
+        let r = response_time(&x, &tasks);
+        println!("{}\nR: {}\n", x, r);
     }
 }
 
 fn get_inp(file: &str, resources: Option<&str>) -> Vec<task::Task> {
     // read in tasks
     let inp = fs::read_to_string(file).expect("unable to read file");
-    let mut tasks = Vec::new();
+
+    let mut task_set = Vec::new();
 
     //skip title line
     for x in inp.lines().skip(1) {
         // split by , since csv
         let mut iter = x.split(',');
 
-        // items should be in this order since users are using  a template
+        // items are in this order since users are using  a template
         let name = iter.next().unwrap().trim().to_string();
 
-        let t = iter.next().unwrap().parse::<u32>().unwrap();
+        let t: f64 = iter.next().unwrap().parse().unwrap();
 
         let d_iter = iter.next().unwrap().trim();
         let d = match d_iter.is_empty() {
-            true => None,
-            false => Some(d_iter.parse().unwrap()),
+            true => t,
+            false => d_iter.parse().unwrap(),
         };
 
         let c = iter.next().unwrap().parse().unwrap();
 
         let p_iter = iter.next().unwrap().trim();
         let p = match p_iter.is_empty() {
-            true => None,
-            false => Some(p_iter.parse().unwrap()),
+            true => 0,
+            false => p_iter.parse().unwrap(),
         };
 
-        let u = c as f64 / t as f64;
+        let u = c / t;
 
+        //TODO: handle resources
         let critical_sections = match resources {
             None => None,
             Some(path) => Some(get_resources(path)),
         };
-        // create task and push to vec
-        tasks.push(task::Task {
+
+        // create task and push to task_set
+        task_set.push(task::Task {
             name,
             T: t,
             D: d,
@@ -56,7 +60,12 @@ fn get_inp(file: &str, resources: Option<&str>) -> Vec<task::Task> {
             critical_sections,
         });
     }
-    tasks
+    if task_set[0].P == 0 {
+        // deadline monotonic ordering is the same as rate monotonic ordering for implicit tasks
+        // as the deadline for implicit tasks is their period
+        deadline_monotonic_ordering(&mut task_set);
+    }
+    task_set
 }
 
 fn get_resources(path: &str) -> Vec<resource::CriticalSection> {
@@ -64,32 +73,56 @@ fn get_resources(path: &str) -> Vec<resource::CriticalSection> {
     v
 }
 
-fn util(tasks: &Vec<task::Task>, families: Option<f64>) -> (f64, f64) {
+fn deadline_monotonic_ordering(task_set: &mut Vec<task::Task>) {
+    task_set.sort_by(|task_a, task_b| task_a.D.partial_cmp(&task_b.D).unwrap());
+    let mut p = task_set.len() as u32;
+    for x in 0..(task_set.len()) {
+        task_set[x].P = p;
+        p -= 1;
+    }
+}
+
+fn rate_monotonic_ordering(task_set: &mut Vec<task::Task>) {
+    task_set.sort_by(|task_a, task_b| task_a.T.partial_cmp(&task_b.T).unwrap());
+    let mut p = task_set.len() as u32;
+    for x in 0..(task_set.len()) {
+        task_set[x].P = p;
+        p -= 1;
+    }
+}
+
+#[allow(non_snake_case)]
+fn LL_utilissation(tasks: &Vec<task::Task>, families: Option<f64>) -> (f64, f64) {
+    // number of families may or may not have been provided
     let n = match families {
         Some(v) => v,
         None => tasks.len() as f64,
     };
-    let max_util = n * ((2 as f64).powf(1 as f64 / n) - 1 as f64);
+
+    let max_util: f64 = n * ((2 as f64).powf(1.0 / n) - 1.0);
     (tasks.iter().map(|x| x.U).sum(), max_util)
 }
 
-fn response(task: &task::Task, tasks: &Vec<task::Task>) -> f64 {
+/// Finds the response time of the given task
+///
+/// # Arguments
+///
+/// `task` is the task you want to analyse
+///
+/// `task_set` is the set off all tasks in the system
+fn response_time(task: &task::Task, tasks: &Vec<task::Task>) -> f64 {
     //get higher priority tasks
-    let higher_p: Vec<task::Task> = tasks
+    let hp: Vec<task::Task> = tasks
         .iter()
         .filter(|x| x.P > task.P)
         .map(|x| x.clone())
         .collect();
 
-    let mut r = task.C as f64;
+    let mut r = task.C;
     let mut last_r = -1.0;
     while r != last_r {
         last_r = r;
-        r = task.C as f64
-            + higher_p
-                .iter()
-                .map(|j| (r as f64 / j.T as f64).ceil() * j.C as f64)
-                .sum::<f64>();
+        r = task.C + hp.iter().map(|j| (r / j.T).ceil() * j.C).sum::<f64>();
     }
     r
 }
--- src/task.rs
+++ src/task.rs
@@ -6,27 +6,40 @@ use std::fmt;
 #[allow(non_snake_case)]
 pub struct Task {
     pub name: String,
-    pub T: u32,
-    pub D: Option<u32>,
-    pub C: u32,
-    pub P: Option<u32>,
+    pub T: f64,
+    pub D: f64,
+    pub C: f64,
+    pub P: u32,
     pub U: f64,
     pub critical_sections: Option<Vec<CriticalSection>>,
 }
 
 impl std::fmt::Display for Task {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "Task {}\nT: {t}\nD: {d:?}\nC: {c}\nP: {p:?}\nU: {u}\nCritical Sections:\n {cr:?}",
-            self.name,
-            t = self.T,
-            d = self.D,
-            c = self.C,
-            p = self.P,
-            u = self.U,
-            cr = self.critical_sections.clone().unwrap(),
-        )
+        let critical_sections = self.critical_sections.clone();
+        match critical_sections {
+            Some(cr) => write!(
+                f,
+                "Task {}\nT: {t}\nD: {d}\nC: {c}\nP: {p}\nU: {u}\nCritical Sections:\n {cr:?}",
+                self.name,
+                t = self.T,
+                d = self.D,
+                c = self.C,
+                p = self.P,
+                u = self.U,
+                cr = cr,
+            ),
+            None => write!(
+                f,
+                "Task {}\nT: {t}\nD: {d}\nC: {c}\nP: {p}\nU: {u}",
+                self.name,
+                t = self.T,
+                c = self.C,
+                d = self.D,
+                p = self.P,
+                u = self.U,
+            ),
+        }
     }
 }
 
